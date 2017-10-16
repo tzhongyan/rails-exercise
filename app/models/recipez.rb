105aa95f9e9ac21330ca23521ff45ae6788539d3
Tan Zhong Yan <iam@tzhongyan.com>
2017-10-16 13:49:47 +0800

    bug fixing

--- app/models/recipez.rb
+++ app/models/recipez.rb
@@ -1,7 +1,7 @@
 class Recipez < ApplicationRecord
     def self.search(title, region)
         if (title.present? && region.present?)
-            where(['lower(title) like ? and lower(region) like ?', "%#{title.downcase}%", "%#{region.downcase}%#"])
+            where(['lower(title) like ? and lower(region) like ?', "%#{title.downcase}%", "%#{region.downcase}%"])
         elsif (title.present?)
             where(['lower(title) like ?', "%#{title.downcase}%"])
         elsif (region.present?)
