00bf14fac0bd4d138ab2e08f55e26cb4458f0fe8
Tan Zhong Yan <iam@tzhongyan.com>
2017-10-16 17:22:18 +0800

    t7

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create({
+    id:11,
+    email:'user@email.com',
+    encrypted_password:'',
+    sign_in_count:0, 
+    created_at:'2007-10-10 10:00.00', 
+    updated_at:'2009-10-10
+    10:00.00', 
+    password:'password'
+})
